0c6f14c7a67dbb1286dd39ca4d5f6be8a660fe63
itzharshit99 <iharshitjain99@gmail.com>
2024-12-20 23:17:57 +0530

    setted up room model

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse;
-# Create your views here.
-def main(request):
-    return HttpResponse("hello")
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
